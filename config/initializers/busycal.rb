9bb6f2c4d0764b3169f36f458f03b1e54863f75a
Rodrigo Tomonari Muino <rodrigo.tomonari@gmail.com>
2017-01-23 15:59:23 -0200

    Configure email settings

--- config/initializers/busycal.rb
+++ config/initializers/busycal.rb
@@ -6,8 +6,22 @@ Rails.env.on(:development) do
   config.file_watcher = ActiveSupport::FileUpdateChecker
 
   config.web_console.whiny_requests = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
 end
 
 Rails.env.on(:production) do
   config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_HOST'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USER'],
+    password:             ENV['SMTP_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.raise_delivery_errors = false
 end
